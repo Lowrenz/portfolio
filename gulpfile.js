5ccaa9a34f00e99657591ae36a13b93445d7058a
Lorenz Gillisjans <lorenz.gillisjans@gmail.com>
2017-07-10 21:53:52 +0200

    FB messenger - form

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,35 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload')
 var uglify = require('gulp-uglifyjs');
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+//var imagemin = require('gulp-imagemin');
+//var pngquant = require('imgylpagemin-pngquant');
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('imagemin', function() {
-    return gulp.src('./img/**/**/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest('./img'));
-});
+// gulp.task('imagemin', function() {
+//     return gulp.src('./img/**/**/*')
+//         .pipe(imagemin({
+//             progressive: true,
+//             svgoPlugins: [{
+//                 removeViewBox: false
+//             }],
+//             use: [pngquant()]
+//         }))
+//         .pipe(gulp.dest('./img'));
+// });
 
-gulp.task('sass', function() {
-    gulp.src('./sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./css'));
-});
+// gulp.task('sass', function() {
+//     gulp.src('./sass/**/*.scss')
+//         .pipe(sourcemaps.init())
+//         .pipe(sass({
+//             outputStyle: 'compressed'
+//         }).on('error', sass.logError))
+//         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+//         .pipe(sourcemaps.write('./'))
+//         .pipe(gulp.dest('./css'));
+// }); 
 
 gulp.src(['js/*.js', '!js/*.min.js'])
 
@@ -47,7 +47,7 @@ gulp.task('minify-html', function() {
 
 gulp.task('watch', function() {
     livereload.listen();
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    //gulp.watch('./sass/**/*.scss', ['sass']);
     //gulp.watch(['./js/*.js', '!./js/*.min.js'], ['uglify']);
     gulp.watch(['./html/**/*.html'], ['minify-html']);
     gulp.watch(['./css/style.css', './js/*.js'], function(files) {
